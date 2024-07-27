6c05c281339d3b4bd914dd1bfc10935f0b16b931
NuclMe <saltansergei@gmail.com>
2024-07-27 23:14:20 +0300

    initial commit

--- src/components/Repos.tsx
+++ src/components/Repos.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 export function Repos() {
-  const repos = useSelector((state) => state.issuesApi.queries);
+  const repos = useSelector((state) => state.issuesApi);
   console.log(repos);
   return <div>Repos</div>;
 }
