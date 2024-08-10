a569f28d67c7877bce48b210fd07eca7d2748563
NuclMe <saltansergei@gmail.com>
2024-08-10 23:43:57 +0300

    added another styles

--- src/components/Repos.tsx
+++ src/components/Repos.tsx
@@ -3,22 +3,28 @@ import { useSelector } from 'react-redux';
 import { Flex } from 'antd';
 import styled from 'styled-components';
 
-const StyledCard = styled(Flex)`
-  width: 33.3%;
-`;
-
 const FlexContainer = styled(Flex)`
   flex-wrap: wrap;
+  margin-right: -20px;
+`;
+const StyledCard = styled(Flex)`
+  width: 33.3%;
+  border: 1px solid #cc8e1e;
+  border-radius: 10px;
+  margin-top: 20px;
+  margin-right: 20px;
+  padding: 15px;
+  background: rgb(246, 245, 242);
 `;
 
 export function Repos() {
   const issuesData = useSelector((state) => state.issuesData.data);
 
   return (
-    <FlexContainer gap="middle">
+    <FlexContainer>
       {issuesData && issuesData.length > 0 ? (
         issuesData.map((issue) => (
-          <StyledCard key={issue.id}>
+          <StyledCard vertical key={issue.id}>
             <span>{issue.title}</span>
             <p>#{issue.number}</p>
           </StyledCard>
