c3bbb9607dad9028fe57ebfd3d91c186a9974656
NuclMe <saltansergei@gmail.com>
2024-08-12 23:50:17 +0300

    made columns

--- src/components/Repos.tsx
+++ src/components/Repos.tsx
@@ -5,14 +5,12 @@ import styled from 'styled-components';
 
 const FlexContainer = styled(Flex)`
   flex-wrap: wrap;
-  margin-right: -20px;
+  padding: 20px;
 `;
 const StyledCard = styled(Flex)`
-  width: 33.3%;
+  flex-basis: calc(33.3% - 15px);
   border: 1px solid #cc8e1e;
   border-radius: 10px;
-  margin-top: 20px;
-  margin-right: 20px;
   padding: 15px;
   background: rgb(246, 245, 242);
 `;
@@ -21,16 +19,18 @@ export function Repos() {
   const issuesData = useSelector((state) => state.issuesData.data);
 
   return (
-    <FlexContainer>
+    <FlexContainer gap="middle">
       {issuesData && issuesData.length > 0 ? (
         issuesData.map((issue) => (
           <StyledCard vertical key={issue.id}>
             <span>{issue.title}</span>
             <p>#{issue.number}</p>
+            <span> {issue.user.login}</span>
+            <span> Comments: {issue.comments}</span>
           </StyledCard>
         ))
       ) : (
-        <p>No issues found</p>
+        <p>No issues found in this repo</p>
       )}
     </FlexContainer>
   );
