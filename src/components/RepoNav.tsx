d4d627c8e2a985ebd5f1ead0e829e2ea0d6ec9f5
NuclMe <saltansergei@gmail.com>
2024-08-29 14:56:27 +0300

    working on links

--- src/components/RepoNav.tsx
+++ src/components/RepoNav.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Flex } from 'antd';
+import { Flex, Anchor } from 'antd';
+import { StarFilled, RightOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
 
 export function RepoNav() {
   const repoInfo = useSelector((state) => state.repoData.data);
-  if (repoInfo) {
-    const starsCount = Math.round(repoInfo.stargazers_count / 1000);
-    return starsCount;
+
+  if (!repoInfo) {
+    return null;
   }
 
+  const StarIcon = styled(StarFilled)`
+    color: #ffd700;
+  `;
+
+  const NavContainer = styled(Flex)`
+    margin-top: 10px;
+  `;
+
+  const starsCount = Math.round(repoInfo.stargazers_count / 1000);
+
   return (
-    repoInfo && (
-      <Flex>
-        <Flex>{repoInfo.organization.login} &gt;</Flex>
-        <Flex> {repoInfo.name} </Flex>
-        <Flex> {repoInfo.stargazers_count}</Flex>
+    <NavContainer gap="middle">
+      <Anchor
+        direction="horizontal"
+        items={[
+          {
+            key: 'part-1',
+            href: `${repoInfo.organization.html_url}`,
+            title: `${repoInfo.organization.login}`,
+          },
+        ]}
+      />
+      <RightOutlined />
+      <Flex>{repoInfo.name}</Flex>
+      <Flex gap="small">
+        <StarIcon />
+        {starsCount} k stars
       </Flex>
-    )
+    </NavContainer>
   );
 }
