46a5046851e07e67ab7fdf7ee9c2a686424fa566
NuclMe <saltansergei@gmail.com>
2024-09-11 13:26:44 +0300

    changed branch name

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { React, StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux';
 import { ApiProvider } from '@reduxjs/toolkit/query/react';
 import { issuesApi } from './redux/issuesApi.ts';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
