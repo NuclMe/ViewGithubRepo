7254528b85b22096fd37fbec7f4a51b84e59e253
NuclMe <saltansergei@gmail.com>
2024-10-06 15:24:12 +0300

    started to add tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import { describe, it } from 'vitest';
+import { describe, it, expect } from 'vitest';
 
-describe('App', () => {
-  it('renders the App component', () => {
-    render(<App />);
-
-    screen.debug(); // prints out the jsx in the App component unto the command line
+describe('A truthy statement', () => {
+  it('should be equal to 2', () => {
+    expect(1 + 1).toEqual(2);
   });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import istanbul from 'vite-plugin-istanbul';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    istanbul({
+      include: 'src/*',
+      exclude: ['node_modules', 'test/'],
+      extension: ['.js', '.ts', '.tsx', '.jsx'],
+      requireEnv: true,
+    }),
+  ],
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: './src/tests/setup.js',
+    coverage: {
+      provider: 'istanbul',
+    },
   },
 });
